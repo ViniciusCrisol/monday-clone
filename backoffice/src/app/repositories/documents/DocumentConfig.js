27196f7938224c941c7b8d2cc7d546c55039817d
Vinícius Crisol <vinicius.crisol@hotmail.com>
2021-04-18 21:19:07 -0300

    projeto: atualização da arquitetura do backoffice

--- backoffice/src/app/repositories/documents/DocumentConfig.js
+++ backoffice/src/app/repositories/documents/DocumentConfig.js
@@ -5,21 +5,24 @@ class DocumentConfig {
   constructor() {
     this.templates = {
       'users-report': {
+        getData: this.getUsersReportData,
         path: this.joinTemplatePath('users_report.ejs'),
-        getData: async (user_id) => {
-          const queryParams = { attributes: ['user_name'] };
-          const response = await Account.findByPk(user_id, queryParams);
-          if (!response) return undefined;
-
-          const { user_name } = response.dataValues;
-          return { name: user_name };
-        },
       },
     };
   }
 
   joinTemplatePath(fileName) {
     return path.join(__dirname, '..', '..', 'views', 'documents', fileName);
+  }
+
+  async getUsersReportData(user_id) {
+    const queryParams = { attributes: ['user_name'] };
+
+    const response = await Account.findByPk(user_id, queryParams);
+    if (!response) return undefined;
+
+    const { user_name } = response.dataValues;
+    return { name: user_name };
   }
 }
 
