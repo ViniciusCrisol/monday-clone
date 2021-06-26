b45b8c3b6bfb2e02e73d29760af34943dc0f6f15
Vinicius Crisol <vinicius.crisol@hotmail.com>
2021-06-26 18:54:49 -0300

    projeto: ajuste ortográfico

--- main-api/src/modules/Members/infra/http/routes/members.routes.ts
+++ main-api/src/modules/Members/infra/http/routes/members.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { celebrate, Segments, Joi } from 'celebrate';
 
-import validuateMemberRole from '@utils/validations/validuateMemberRole';
+import validateMemberRole from '@utils/validations/validateMemberRole';
 import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
 import UpdateMemberRolesController from '../controllers/UpdateMemberRolesController';
 
@@ -13,7 +13,7 @@ membersRoutes.patch(
   celebrate({
     [Segments.PARAMS]: { id: Joi.string().uuid() },
     [Segments.BODY]: {
-      role: Joi.string().regex(validuateMemberRole).required(),
+      role: Joi.string().regex(validateMemberRole).required(),
     },
   }),
   ensureAuthenticated,
